87ffbd3b8e61619327bf0d5f2b49af8b1ecf8af2
gülşen yıldız <gulsenylddz@gmail.com>
2024-12-09 16:03:00 +0300

    fronr-changes

---
+++ HemenBilet/wwwroot/frontend/login/login.js
@@ -0,0 +1,23 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const registerModal = document.getElementById("registerModal");
+    const openRegister = document.getElementById("openRegister");
+    const closeModal = document.querySelector(".close");
+
+    // Modalı Açma
+    openRegister.addEventListener("click", (e) => {
+        e.preventDefault(); // Sayfa yenilenmesini engelle
+        registerModal.style.display = "block"; // Modal görünür hale gelir
+    });
+
+    // Modalı Kapatma
+    closeModal.addEventListener("click", () => {
+        registerModal.style.display = "none";
+    });
+
+    // Modal dışında bir yere tıklanınca kapat
+    window.addEventListener("click", (e) => {
+        if (e.target === registerModal) {
+            registerModal.style.display = "none";
+        }
+    });
+});
